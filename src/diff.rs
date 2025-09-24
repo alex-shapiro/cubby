285bcc9c1783ff55104e829facd9d97bbb21aad0
Alex Shapiro <alex@alexshapiro.xyz>
2025-09-24 12:50:06 -0400

    make diff struct fields private outside of the crate

--- src/diff.rs
+++ src/diff.rs
@@ -11,8 +11,8 @@ pub struct DiffRequest(pub(crate) HashMap<PeerId, DiffRequestPeerState>);
 #[derive(Serialize, Deserialize)]
 pub struct DiffRequestPeerState {
     #[serde(skip_serializing_if = "RoaringTreemap::is_empty")]
-    pub index: RoaringTreemap,
-    pub bookmark: Hlc,
+    pub(crate) index: RoaringTreemap,
+    pub(crate) bookmark: Hlc,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -21,17 +21,17 @@ pub struct Diff<K, V>(pub(crate) HashMap<PeerId, DiffPeerState<K, V>>);
 #[derive(Serialize, Deserialize)]
 pub struct DiffPeerState<K, V> {
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub inserts: Vec<Insert<K, V>>,
+    pub(crate) inserts: Vec<Insert<K, V>>,
     #[serde(skip_serializing_if = "RoaringTreemap::is_empty")]
-    pub deletes: RoaringTreemap,
-    pub bookmark: Hlc,
+    pub(crate) deletes: RoaringTreemap,
+    pub(crate) bookmark: Hlc,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Insert<K, V> {
-    pub key: K,
-    pub value: V,
-    pub hlc: Hlc,
+    pub(crate) key: K,
+    pub(crate) value: V,
+    pub(crate) hlc: Hlc,
 }
 
 impl DiffRequest {
